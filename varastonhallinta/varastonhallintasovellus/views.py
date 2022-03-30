03e21160cd59804d6f4002988980f8cd832ba990
JuhaniRusi <juhanirusi@yahoo.com>
2022-03-30 13:18:49 +0300

    Lisätty näkymät sivuille.

--- varastonhallinta/varastonhallintasovellus/views.py
+++ varastonhallinta/varastonhallintasovellus/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import (
+    HttpResponse, 
+    HttpResponseBadRequest,
+    HttpResponseNotFound)
+
+from django.shortcuts import render
+
+#from .models import Henkilo
+
+def kirjautuminen(request):
+    return render(request, 'kirjautuminen.html')
+
+
+def tuotehaku(request):
+    return HttpResponse('tuotehaku')
+
+
+def lainaus(request):
+    return HttpResponse('lainaussivu')
+
+
+def palautus(request):
+    return HttpResponse('palautussivu')
+
+
+def hallinta(request):
+    return HttpResponse('hallinta')
+
+
+def lisaaminen(request):
+    return HttpResponse('tuotteiden lisääminen')
