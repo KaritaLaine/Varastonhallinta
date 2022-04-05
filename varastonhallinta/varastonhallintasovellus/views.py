7bc9cebb067ccabc3c68c21e158619d91ba1b2ab
Juhani <juhanirusi@yahoo.com>
2022-04-05 14:57:37 +0300

    Tehty kirjautuminen tunnukilla mahdolliseksi "kirjautuminen.html" sivun kautta.

--- varastonhallinta/varastonhallintasovellus/views.py
+++ varastonhallinta/varastonhallintasovellus/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+#from django.contrib.auth import login, logout
 
 # from django.http import (
 #     HttpResponse, 
 #     HttpResponseBadRequest,
 #     HttpResponseNotFound)
 
-from django.shortcuts import render
-
 #from .models import Henkilo
 
 def kirjautuminen(request):
-    return render(request, 'kirjautuminen.html')
+    if request.method == 'POST':
+        username = request.POST['käyttäjätunnus']
+        password = request.POST['salasana']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('hallinta/')
+        else:
+            messages.success(request, ("Antamasi salasana tai käyttäjätunnus on väärä!"))
+            return redirect('/')
+    else:
+        return render(request, 'kirjautuminen.html')
 
 
 # def tuotehaku(request):
