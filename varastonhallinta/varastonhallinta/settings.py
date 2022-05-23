3bf4612898e97f3655ed1b28dcbff332b84feeb2
anne <43341@edu.raseko.fi>
2022-05-23 09:37:44 +0300

    Päivitetty 404 sivu

--- varastonhallinta/varastonhallinta/settings.py
+++ varastonhallinta/varastonhallinta/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#2^&*9*#yw5p#vbar1vyex3z$3h0co72%$nimccfi72^k4_mb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
