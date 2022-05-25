bb27c551723b6258fe054f8495fb1f946e1f62aa
anne <43341@edu.raseko.fi>
2022-05-25 13:43:31 +0300

    Tuotteen poisto

--- varastonhallinta/varastonhallinta/settings.py
+++ varastonhallinta/varastonhallinta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#2^&*9*#yw5p#vbar1vyex3z$3h0co72%$nimccfi72^k4_mb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
